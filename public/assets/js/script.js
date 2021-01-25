0d242705df0d70f4e2af405afa354138944fe612
Jacci Horvath <jaccinhorvath@gmail.com>
2021-01-25 18:00:52 -0500

    adding public folder

---
+++ public/assets/js/script.js
@@ -0,0 +1,39 @@
+$(function () {
+    $('#devour-btn').on('click', function (event) {
+        let id = $(this).data('id');
+        let isDevoured = $(this).data('devoured');
+
+        let nowDevoured = {
+            devoured: isDevoured
+        };
+
+        $.ajax('/api/burgers/' + id, {
+            type: 'PUT',
+            data: nowDevoured
+        }).then(
+            function () {
+                console.log('this burger is now devoured!');
+                location.reload();
+            }
+        );
+    });
+
+    $('#add').on('click', function (event) {
+        event.preventDefault();
+
+        let newBurger = {
+            name: $('#burger').val().trim(),
+            devoured: false
+        };
+
+        $.ajax('/api/burgers', {
+            type: 'POST',
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+                location.reload();
+            }
+        );
+    });
+});

0c86d48cf0c5f519efd4e9df0c54b4783aaa4828
Jacci Horvath <jaccinhorvath@gmail.com>
2021-01-25 18:00:05 -0500

    adding db folder

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO burgers (burger_name, devoured) VALUES
+    ('Southwest Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES
+    ('All-American Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES
+    ('Bacon Cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES
+    ('Chili Cheese Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES
+    ('The Bleu Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES
+    ('Veggie Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES
+    ('The Impossible Burger', false);

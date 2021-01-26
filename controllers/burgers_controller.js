d60e8a6bd6669b6451450a9161c44a0455fa565d
Jacci Horvath <jaccinhorvath@gmail.com>
2021-01-25 19:54:07 -0500

    fixing res.render

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,7 +12,7 @@ router.get('/', function (req, res) {
             burgers: data
         };
         console.log(obj);
-        res.render('index', obj);
+        res.render('index.handlebars', obj);
     });
 });
 
